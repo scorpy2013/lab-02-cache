feb063c1a6fe916bb030cca5cb7edf348681d7a6
alexscorpy <scorpy2013@gmail.com>
2020-10-16 17:11:10 +0300

    Final test

---
+++ sources/lab2.cpp
@@ -0,0 +1,19 @@
+// Copyright 2018 Your Name <your_email>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <vector>
+
+//#include "gtest/gtest.h"
+#include "analytics.hpp"
+
+int main() {
+  Analytics final_tests;
+  final_tests.Start();
+  final_tests.Report();
+  return 0;
+}
